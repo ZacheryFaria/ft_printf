afc83155f487570c304abef9766156e545257232
Zachery <zacheryfaria@gmail.com>
2019-01-29 15:11:11 -0800

    Added useless variable in ft_lstdel

--- libft/src/ft_lstdel.c
+++ libft/src/ft_lstdel.c
@@ -15,11 +15,8 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	t_list *next;
-
 	while (*alst)
 	{
-		next = (*alst)->next;
 		del((*alst)->content, (*alst)->content_size);
 		free(*alst);
 		*alst = (*alst)->next;

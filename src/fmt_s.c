11f75a6932eb0c97b6d822b48e787fb09150fdb1
zacheryfaria <zacheryfaria@gmail.com>
2019-04-11 16:24:38 -0700

    Fmt s is done.

--- src/fmt_s.c
+++ src/fmt_s.c
@@ -3,15 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   fmt_s.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: z <z@student.42.fr>                        +#+  +:+       +#+        */
+/*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 13:59:04 by z                 #+#    #+#             */
-/*   Updated: 2019/02/01 14:00:21 by z                ###   ########.fr       */
+/*   Updated: 2019/04/11 16:06:09 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*fmt_s(char *fmt, char *str)
+#include <libftprintf.h>
+#include <stdio.h>
+#include <libft.h>
+
+char		*handle_padding(t_fmtarg *arg, char *str)
 {
-	fmt[0] = str[0];
-	return (fmt);
+	char	*news;
+	int		len;
+
+	len = ft_strlen(str);
+	if (arg->padding > len)
+	{
+		news = ft_memalloc(arg->padding + 1);
+		ft_memset(news, ' ', arg->padding);
+		if (arg->leftalign)
+			ft_memcpy(news, str, len);
+		else
+			ft_memcpy(news + (arg->padding - len), str, len);
+		free(str);
+	}
+	else
+	{
+		return (str);
+	}
+	return (news);
+}
+
+char		*handle_precision(t_fmtarg *arg, char *str)
+{
+	if (arg->precisionb)
+	{
+		if (arg->precision < ft_strlen(str))
+		{
+			str[arg->precision] = 0;
+		}
+	}
+	return (str);
+}
+
+t_result	*fmt_s(t_fmtarg *arg, va_list varg)
+{
+	t_result	*res;
+	char		*astr;
+
+	(void)arg;
+	res = malloc(sizeof(t_result));
+	astr = va_arg(varg, char *);
+	if (astr == 0)
+	{
+		res->str = ft_strdup("(null)");
+		res->bytes = ft_strlen(res->str);
+	}
+	else
+		res->str = ft_strdup(astr);
+	res->str = handle_precision(arg, res->str);
+	res->str = handle_padding(arg, res->str);
+	res->bytes = ft_strlen(res->str);
+	return (res);
 }

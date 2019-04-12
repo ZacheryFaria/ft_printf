82d4dfd85e1807dd163673b6a74fce80e96c2ebc
zacheryfaria <zacheryfaria@gmail.com>
2019-04-12 11:42:39 -0700

    Made itoa a bit better

--- libft/src/string/ft_itoa.c
+++ libft/src/string/ft_itoa.c
@@ -6,21 +6,22 @@
 /*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 14:25:56 by zfaria            #+#    #+#             */
-/*   Updated: 2019/03/05 12:55:46 by awindham         ###   ########.fr       */
+/*   Updated: 2019/04/12 11:41:57 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+char	*ft_itoa(int64_t n)
 {
 	char			*res;
 	int				i;
-	unsigned int	num;
+	uint64_t		num;
 
 	if (n == 0 && !(i = 0))
 		return (ft_strdup("0"));
-	res = ft_strnew(13);
+	res = ft_strnew(33);
+	i = 0;
 	if (res)
 	{
 		if (n < 0)
